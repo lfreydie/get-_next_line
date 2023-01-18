f37b81829f92c1350dbc00118f4a158bca3052a3
Leonard FREYDIER <lfreydie@e3r4p14.clusters.42paris.fr>
2023-01-18 17:40:51 +0100

    final

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lfreydie <lfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 09:55:59 by lefreydier        #+#    #+#             */
-/*   Updated: 2023/01/18 15:20:00 by lfreydie         ###   ########.fr       */
+/*   Updated: 2023/01/18 17:40:10 by lfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,6 @@ int	ft_strchr(char *str, char c)
 	return (0);
 }
 
-// char	*ft_strdup(const char *s)
-// {
-// 	char	*res;
-// 	int		len_s;
-// 	int		i;
-
-// 	i = 0;
-// 	len_s = ft_strlen(s);
-// 	res = malloc(sizeof(*res) * (len_s + 1));
-// 	if (!res)
-// 		return (NULL);
-// 	while (s[i])
-// 	{
-// 		res[i] = s[i];
-// 		i++;
-// 	}
-// 	res[i] = 0;
-// 	return (res);
-// }
-
 size_t	ft_strlen(const char *s)
 {
 	int	i;
@@ -59,32 +39,6 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
-
-// char	*ft_strjoin(char *s1, char *s2)
-// {
-// 	char	*new;
-// 	int		i;
-// 	int		j;
-
-// 	if (!s1 && !s2)
-// 		return (free(s1), NULL);
-// 	else if (!s1)
-// 		new = ft_strdup(s2);
-// 	else
-// 	{
-// 		new = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-// 		if (!new)
-// 			return (free(s1), NULL);
-// 		j = 0;
-// 		while (s1[j])
-// 			new[i++] = s1[j++];
-// 		j = 0;
-// 		while (s2[j])
-// 			new[i++] = s2[j++];
-// 		new[i] = '\0';
-// 	}
-// 	return (free(s1), new);
-// }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
